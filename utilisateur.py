98578f072f02ea80056911ebe283fbd841df9d0b
Maokan <hugo.speter@hotmail.com>
2025-10-17 10:23:15 +0200

    Squashed commit of the following:
    commit a4d7f122b287e9ef2f6452f283720519b90d2da7
    Author: SB <swannbuissieres@gmail.com>
    Date:   Fri Oct 17 10:18:43 2025 +0200
    
        update utilisateur.py
    
        added login and register
    
    commit 9569410ada01113d1149bc1810b4d7f46d3daad3
    Author: SB <swannbuissieres@gmail.com>
    Date:   Tue Oct 14 14:29:49 2025 +0200
    
        User.py created

--- utilisateur.py
+++ utilisateur.py
@@ -1,37 +1,104 @@
-    from fastapi import FastAPI
-    app = FastAPI()
-    @app.get("/")
-    def read_root():
-        return {"message": "Bienvenue sur FastAPI!"}
-def register():
-    names=[]
-    usernames=[]
-    passwords=[]
-    names.append(input("Enter your name:"))
-    usernames.append(input("choose your username:"))
-    passwords.append(input("choose your password:"))
-    return usernames
-def login(usernames,passwords):
-    usernames=[]
-    passwords=[]
-    password=""
-    username=""
-    username=input("Enter your username:")
-    password=input("Enter your Password:")
-    if password==passwords:#[usernames.index(username)]:
-       print("welcome")
-    else:
-       print("incorrect!")
+#fastapi dev utilisateur.py
 
-account_ans=""
-while True:
-    account_ans=input("choose:  a)Sign Up     b)login and shop     c)quit")
-    if account_ans=="a":
-       register()
-    if account_ans=="b":
-       password=""
-       username=""
-       usernames=[]
-       passwords=[]
-       login(usernames,passwords)
-    if account_ans=="c":break
+from datetime import datetime, timedelta
+from fastapi import Depends, FastAPI, HTTPException, Security
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Session, create_engine, select
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from main import User
+import bcrypt
+import jwt
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+secret_key = "SLVM"
+Algorithm = "HS256"
+security = HTTPBearer(auto_error=False)
+global user_id
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+app = FastAPI()
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+class CreateUser(BaseModel):
+    username:str
+    adress_mail:str
+    password:str
+    name:str
+    first_name:str
+
+class GetUser(BaseModel):
+    adress_mail:str
+    password:str
+
+class GetUserInfos(BaseModel):
+    username:str
+    adress_mail:str
+    name:str
+    first_name:str
+
+@app.post("/users/")
+def create_user(body: CreateUser, session = Depends(get_session)) -> User:
+    user = User(name=body.name)
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+
+@app.post("/getUser/")
+def create_item (getUser:GetUser)-> GetUser:
+    return GetUser
+    
+@app.post("/register")
+def register(body: CreateUser, session: Session = Depends(get_session)):
+    print(body.password)
+    chiffrement = bcrypt.hashpw(body.password.encode('utf-8'), bcrypt.gensalt())
+    user = User(name=body.name,username=body.username,adress_mail=body.adress_mail,password=chiffrement,first_name=body.first_name)
+         # Vérifie si l'utilisateur existe déjà
+    statement = select(User).where(User.adress_mail == body.adress_mail)
+    existing_user = session.exec(statement).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Un utilisateur ayant cette adresse email est déjà enrengistré")
+    else:
+        print(chiffrement)
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+    return user
+@app.get("/users")
+def users(session = Depends(get_session)):
+    users = session.exec(select(User)).all()
+    return users
+
+@app.post("/login")
+def login(body:GetUser, session:Session = Depends(get_session)):
+    # Recherche de l'utilisateur
+    statement = select(User).where(User.adress_mail == body.adress_mail)
+    mail = session.exec(statement).first()
+    if not mail:
+        raise HTTPException(status_code=404, detail="Adresse mail incorrecte")
+    # Vérifie le mot de passe
+    if not bcrypt.checkpw(body.password.encode('utf-8'), mail.password):
+        raise HTTPException(status_code=401, detail="Mot de passe incorrecte")
+
+    global user_id
+    user_id = mail.id
+    return {"message": f"Welcome back, {mail.username} !"}
+
+@app.get("/infos", response_model=GetUserInfos)
+def infos(session:Session = Depends(get_session)):
+        user = session.get(User, user_id)
+
+        return user
